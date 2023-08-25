6c34f724fe2b9fd6a523fba3c657f036fad92ee9
Nazar <nazar_pilipenko@inbox.ru>
2023-08-25 18:32:24 +0300

    first commit

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,13 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+    return (
+        <input 
+            type="text" 
+            className="form-control search-input"
+            placheholder="Найти сотрудника"
+            />
+    )
+}
+
+export default SearchPanel;
