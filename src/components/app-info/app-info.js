f17542c17336022ff95e3a061f371f4fde92804a
Nazar <nazar_pilipenko@inbox.ru>
2023-09-06 22:18:05 +0300

    add counter employers

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,11 @@
 import './app-info.css';
 
-const AppInfo = () => {
+const AppInfo = ({increased, employers}) => {
     return (
         <div className="app-info">
-            <h1>Учет сотрудников в компании Н</h1>
-            <h2>Общее число сотрудников</h2>
-            <h2>Премии получат: </h2>
+            <h1>Учет сотрудников в компании </h1>
+            <h2>Общее число сотрудников {employers}</h2>
+            <h2>Премии получат: {increased}</h2>
         </div>
     )
 }
