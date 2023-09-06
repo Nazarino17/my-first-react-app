64928a1abe431250b100419121c51dfa1098d4e4
Nazar <nazar_pilipenko@inbox.ru>
2023-09-06 21:18:42 +0300

    add func delet item

--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -1,17 +1,22 @@
 import './employers-list.css';
-
 import EmployersListItem from "../employers-list-item/employers-list-item";
 
-const EmployersList = ({data}) => {
+const EmployersList = ({data, onDelete}) => {
 
     const elements = data.map(item => {
+        const {id, ...itemProps} = item; // диструктуризация, вітаскиваем айди
         return (
-            <EmployersListItem  {...item}/>
+            <EmployersListItem 
+                key={id} 
+                {...itemProps}
+                onDelete={() => onDelete(id)}/>
             //name={item.name} salary={item.salary} или так
         )
     })
 
+
     return (
+        
         <ul className="app-list list-group">
             {elements}
         </ul>
